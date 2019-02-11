da1632a32b243ac0233af1f60ec9c2c202fe65b4
Ziyue Meng <z.y.meng@berkeley.edu>
2019-02-11 10:48:22 -0800

    Update bit_ops.c

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -6,22 +6,22 @@ unsigned get_bit(unsigned x,
                  unsigned n) {
     // YOUR CODE HERE
     // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
+    // no sense, because get_bit only returns
     // 0 or 1)
-    return -1;
+    return 1 & (x >> n);
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    return (*x & ~(1 << n)) | ((v << n));
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    return (*x ^ (1 << n));
 }
 
 
